82a25b50441c7c532f0d81f37b6d3df4f0dbe622
Amir Ezzati <amezzati9@gmail.com>
2023-01-17 04:17:24 +0330

    add symptoms to database

--- Diagnosis_by_symptoms/Diagnosis_by_symptoms/settings.py
+++ Diagnosis_by_symptoms/Diagnosis_by_symptoms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'diagnosis.apps.DiagnosisConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Diagnosis_by_symptoms/diagnosis/admin.py
+++ Diagnosis_by_symptoms/diagnosis/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Symptom
+
+admin.site.register(Symptom)
