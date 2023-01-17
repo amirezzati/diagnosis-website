4b47caeb7afd765fd31910397232c215a58e28c1
Amir Ezzati <amezzati9@gmail.com>
2023-01-17 17:51:16 +0330

    add name_en column into Symptom model

--- Diagnosis_by_symptoms/diagnosis/models.py
+++ Diagnosis_by_symptoms/diagnosis/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Symptom(models.Model):
     name = models.CharField(max_length=100)
+    name_en = models.CharField(max_length=100, default='')
     
     def __str__(self):
         return self.name
