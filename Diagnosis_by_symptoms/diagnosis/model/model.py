8f12cc4842738b06a027679113e692d1de1ce78a
Amir Ezzati <amezzati9@gmail.com>
2023-01-18 18:33:57 +0330

    add persian name of diseases to model

--- Diagnosis_by_symptoms/diagnosis/model/model.py
+++ Diagnosis_by_symptoms/diagnosis/model/model.py
@@ -11,10 +11,54 @@ file_ = open(classList_loc, "rb")
 output_classes = pickle.load(file_)
 file_.close()
 
+persion_disease = {
+    '(vertigo) Paroymsal  Positional Vertigo': '(سرگیجه) سرگیجه موضعی پارویمسال',
+    'AIDS': 'ایدز',
+    'Acne': 'آکنه',
+    'Alcoholic hepatitis': 'هپاتیت الکلی',
+    'Allergy': 'آلرژی',
+    'Arthritis': 'آرتروز',
+    'Bronchial Asthma': 'آسم برونش',
+    'Cervical spondylosis': 'اسپوندیلوز گردنی',
+    'Chicken pox': 'آبله مرغان',
+    'Chronic cholestasis': 'کلستاز مزمن',
+    'Common Cold': 'سرماخوردگی',
+    'Dengue': 'دنگی',
+    'Diabetes ': 'دیابت',
+    'Dimorphic hemmorhoids(piles)': 'هموروئید دو شکل (شمع)',
+    'Drug Reaction': 'واکنش دارویی',
+    'Fungal infection': 'عفونت قارچی',
+    'GERD': 'ریفلاکس معده یا مری',
+    'Gastroenteritis': 'گاستروانتریت (آنفلوانزای معده)',
+    'Heart attack': 'حمله قلبی',
+    'hepatitis A': 'هپاتیت A',
+    'Hepatitis B': 'هپاتیت B',
+    'Hepatitis C': 'هپاتیت C',
+    'Hepatitis D': 'هپاتیت D',
+    'Hepatitis E': 'هپاتیت E',
+    'Hypertension ': 'فشار خون',
+    'Hyperthyroidism': 'پرکاری تیروئید',
+    'Hypoglycemia': 'هیپوگلیسمی',
+    'Hypothyroidism': 'کم کاری تیروئید',
+    'Impetigo': 'زرد زخم',
+    'Jaundice': 'زردی',
+    'Malaria': 'مالاریا',
+    'Migraine': 'میگرن',
+    'Osteoarthristis': 'آرتروز',
+    'Paralysis (brain hemorrhage)': 'فلج (خونریزی مغزی)',
+    'Peptic ulcer diseae': 'زخم پپتیک',
+    'Pneumonia': 'ذات الریه',
+    'Psoriasis': 'پسوریازیس',
+    'Tuberculosis': 'سل',
+    'Typhoid': 'حصبه',
+    'Urinary tract infection': 'عفونت مجاری ادراری',
+    'Varicose veins': 'رگ های واریسی'
+}
+
 
 def predict(features):
     model = load_model(model_loc)
     result = model.predict(np.array([features]), verbose=0)
     class_num = np.argmax(result)
 
-    return output_classes[class_num]
+    return persion_disease[output_classes[class_num]]
